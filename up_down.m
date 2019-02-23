aaa44d43deff782e3214e7307995076ba0d29c74
Dalia Tariq <daliatariq95@gmail.com>
2019-02-22 23:33:06 -0800

    code files added

---
+++ up_down.m
@@ -0,0 +1,28 @@
+clc;
+clear all;
+close all;
+%continuous sinusoidal signal
+a=input('Enter the amplitude:');
+f=input('Enter the Timeperiod:');
+t=-10:1:20;
+x=a*sin(2*pi*f*t);
+subplot(3,1,1);
+plot(t,x);
+xlabel('time');
+ylabel('Amplitude');
+title('Sinusoidal signal');
+
+%downsampling the signal
+y4=downsample(x,2);
+subplot(3,1,2);
+stem(y4);
+xlabel('time');
+ylabel('Amplitude');
+title('Downsampled signal');
+%upsampling the signal
+y5=upsample(x,3);
+subplot(3,1,3);
+stem(y5);
+xlabel('time');
+ylabel('Amplitude');
+title('Upsampled signal');
